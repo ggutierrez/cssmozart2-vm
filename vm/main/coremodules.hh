f8ed93ad23b885896433f7094a28421abf11ea15
miavisa <miavisa@gmail.com>
2013-02-19 10:42:45 -0500

    Removed modsearch.hh Gecode error after merge

--- vm/main/coremodules.hh
+++ vm/main/coremodules.hh
@@ -62,7 +62,6 @@
 #include "modules/modvirtualbytestring.hh"
 #include "modules/modvirtualstring.hh"
 #include "modules/modintvar.hh"
-#include "modules/modsearch.hh"
 #include "modules/modfdb.hh"
 
 #endif // __COREMODULES_H
